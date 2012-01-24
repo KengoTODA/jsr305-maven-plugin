91e017a917fcebeb0526a048d8b05964313208a8
eller86 <skypencil@gmail.com>
2012-01-24 09:15:15 +0800

    add test cases for 0 and +1

--- src/test/java/jp/skypencil/jsr305/negative/IntNegativeCheckTest.java
+++ src/test/java/jp/skypencil/jsr305/negative/IntNegativeCheckTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertThat;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.junit.Assert;
+
 import jp.skypencil.jsr305.Scope;
 import jp.skypencil.jsr305.negative.NegativeCheckLevel;
 
@@ -21,6 +23,13 @@ public class IntNegativeCheckTest extends NegativeCheckTest {
 			Class<? extends Throwable> exception)
 			throws IllegalArgumentException, IllegalAccessException {
 		method.setAccessible(true);
+		try {
+			method.invoke(instance, new Object[] { 0 });
+			method.invoke(instance, new Object[] { 1 });
+		} catch (InvocationTargetException e) {
+			Assert.fail();
+		}
+
 		try {
 			method.invoke(instance, new Object[] { -1 });
 			return false;
--- src/test/java/jp/skypencil/jsr305/negative/LongNegativeCheckTest.java
+++ src/test/java/jp/skypencil/jsr305/negative/LongNegativeCheckTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertThat;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.junit.Assert;
+
 import jp.skypencil.jsr305.Scope;
 import jp.skypencil.jsr305.negative.NegativeCheckLevel;
 
@@ -21,6 +23,13 @@ public class LongNegativeCheckTest extends NegativeCheckTest {
 			Class<? extends Throwable> exception)
 			throws IllegalArgumentException, IllegalAccessException {
 		method.setAccessible(true);
+		try {
+			method.invoke(instance, new Object[] { 0L });
+			method.invoke(instance, new Object[] { 1L });
+		} catch (InvocationTargetException e) {
+			Assert.fail();
+		}
+
 		try {
 			method.invoke(instance, new Object[] { -1L });
 			return false;
