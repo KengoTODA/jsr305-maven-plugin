23df32e1adad6f515c068bed5c3815ca8d178234
eller86 <skypencil@gmail.com>
2012-06-01 22:09:03 +0800

    adding javadoc for test package

--- src/test/java/jp/skypencil/jsr305/nullable/package_annotated_with_nonnull/package-info.java
+++ src/test/java/jp/skypencil/jsr305/nullable/package_annotated_with_nonnull/package-info.java
@@ -1,3 +1,11 @@
+/**
+ * <p>This package is created to test our product with &#064;ParametersAreNonnullByDefault annotation.
+ * <ul>
+ *   <li>How PackageInfoBuilder works
+ *   <li>How injection works with &#064;ParametersAreNonnullByDefault
+ *   <li>How injection works with &#064;ParametersAreNullableByDefault which overwrites &#064;ParametersAreNonnullByDefault
+ * </ul>
+ */
 @ParametersAreNonnullByDefault
 package jp.skypencil.jsr305.nullable.package_annotated_with_nonnull;
 
--- src/test/java/jp/skypencil/jsr305/nullable/package_annotated_with_nullable/package-info.java
+++ src/test/java/jp/skypencil/jsr305/nullable/package_annotated_with_nullable/package-info.java
@@ -1,5 +1,10 @@
 /**
- * Package for testing PackageInfoBuilder
+ * <p>This package is created to test our product with &#064;ParametersAreNullableByDefault annotation.
+ * <ul>
+ *   <li>How PackageInfoBuilder works
+ *   <li>How injection works with &#064;ParametersAreNullableByDefault
+ *   <li>How injection works with &#064;ParametersAreNonnullByDefault which overwrites &#064;ParametersAreNullableByDefault
+ * </ul>
  */
 @ParametersAreNullableByDefault
 package jp.skypencil.jsr305.nullable.package_annotated_with_nullable;
